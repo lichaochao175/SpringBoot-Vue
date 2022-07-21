9ed36cf429024f6f8892959e55219ed67e99f99e
lichaochao <1035508926@qq.com>
2022-07-21 21:15:10 +0800

    测试

--- src/main/java/com/lcc/springbootvue/controller/RabbitMQController.java
+++ src/main/java/com/lcc/springbootvue/controller/RabbitMQController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 /**
  * @author lichaochao
- * @data 2022/7/7 8:36 AM
+ * data 2022/7/7 8:36 AM
  **/
 @RestController
 @RequestMapping("mq")
@@ -21,28 +21,29 @@ public class RabbitMQController {
     }
 
     @RequestMapping("/multiSend")
-    public String multiSend(){
-        StringBuilder times=new StringBuilder();
-        for(int i=0;i<1000;i++){
-            long time=System.nanoTime();
-            rabbitTemplate.convertAndSend("sss","第"+i+"次发送的时间："+time);
-            times.append(time+"<br>");
+
+
+    public String multiSend() {
+        StringBuilder times = new StringBuilder();
+        for (int i = 0; i < 1000; i++) {
+            long time = System.nanoTime();
+            rabbitTemplate.convertAndSend("sss", "第" + i + "次发送的时间：" + time);
+            times.append(time + "<br>");
         }
         return times.toString();
     }
 
     @PostMapping("push")
-    public String push(@RequestBody Order order){
-        rabbitTemplate.convertAndSend("sss",order);
+    public String push(@RequestBody Order order) {
+        rabbitTemplate.convertAndSend("sss", order);
         return "ssss";
     }
 
     @GetMapping("getAlll")
-    public String getAll(){
+    public String getAll() {
         MessageConverter messageConverter = rabbitTemplate.getMessageConverter();
         return "ssss";
     }
-
 
 
 }
